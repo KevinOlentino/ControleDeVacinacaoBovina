9b4432ca1bb78069c05443164e43394235240687
Kevin Olentino <kevinolentino@gmail.com>
2021-11-18 00:55:29 -0400

    Modelando telas

--- FrontEnd - ControleDeVacinacaoBovina/angular-controle-de-vacinacao-bovina/src/app/app-routing.module.ts
+++ FrontEnd - ControleDeVacinacaoBovina/angular-controle-de-vacinacao-bovina/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ProdutorComponent } from './../componentes/produtor/produtor.component';
+import { VendaComponent } from './../componentes/venda/venda.component';
+import { PropriedadeComponent } from './../componentes/propriedade/propriedade.component';
+import { RegistrovacinaComponent } from './../componentes/registrovacina/registrovacina.component';
+import { AnimalComponent } from './../componentes/animal/animal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "Animal", component: AnimalComponent},
+  {path: "RegistroVacina", component: RegistrovacinaComponent},
+  {path: "Propriedade", component: PropriedadeComponent},
+  {path: "Venda", component: VendaComponent},
+  {path: "Produtor", component: ProdutorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
