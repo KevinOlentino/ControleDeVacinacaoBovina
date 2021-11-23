0a34cfe4ce372061b08a9097613a99f9b5c02f1a
Kevin Olentino <kevinolentino@gmail.com>
2021-11-22 22:06:36 -0400

    Criando ENUM para os erros de HTTP

---
+++ BackEnd - ControleDeVacinacaoBovina/Models/Dtos/EStatusCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeVacinacaoBovina.Models.Dtos
+{
+    public enum EStatusCode
+    {
+        OK = 200,
+        BAD_REQUEST = 400,
+        NOT_FOUND = 404,
+        NO_CONTENT = 204,
+        INTERNAL_SERVER_ERROR = 500
+    }
+}
